0e3141d530c86a7f4f3fdc865a5942b830942f48
zzzRYT <117448747+zzzRYT@users.noreply.github.com>
2023-06-06 21:33:06 +0900

    m3_js추가_230606

---
+++ js/m1.js
@@ -0,0 +1,4 @@
+function change_color(color) {
+    document.getElementById('thema').contentWindow.document.querySelector('body').style.backgroundColor = color;
+    let ctDiv = document.createElement('div');
+}
--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ function input() {
     const dday = new Date(document.querySelector('#input_date').value);
     const passedTime = now.getTime() - dday.getTime();
     const passedDay = Math.round(passedTime/(24*60*60*1000));
-    document.querySelector('#love').innerHTML = passedDay + '일';
+    document.querySelector('#love').innerHTML = passedDay;
 }
